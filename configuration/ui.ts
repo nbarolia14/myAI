bf4ea5b9971dc896d284d738a8d70f999a1b9b34
nbarolia14 <nehabarolia14@gmail.com>
2025-02-24 21:26:38 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by NB @UNC`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_TITLE: string = `Neha Barolia's AI assistant`;
+export const PAGE_DESCRIPTION: string = `Chat with Meera, Neha Barolia's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
