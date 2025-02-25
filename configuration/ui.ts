682e119704243c0de6b15f29a324eb9e0dad5ca1
nbarolia14 <nehabarolia14@gmail.com>
2025-02-25 05:37:00 +0000

    Changed Chat header

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Meera`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by NB @UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
 export const PAGE_TITLE: string = `Neha Barolia's AI assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with Meera, Neha Barolia's AI assistant.`;
 
